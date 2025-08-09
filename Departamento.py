48dc8eda147f9eb9f3fbe089e45f00e78fc3f998
albademichele <alba.demichele@correo.unimet.edu.ve>
2025-08-08 23:21:21 -0400

    Update Departamento.py

--- Departamento.py
+++ Departamento.py
@@ -1,7 +1,8 @@
 class Departamento:
     def __init__(self, department_id, display_name):
-        self.department_id = id
+        self.department_id = department_id
         self.display_name = display_name
 
     def show(self):
         print(f"ID: {self.department_id} - Departamento: {self.display_name}")
+
